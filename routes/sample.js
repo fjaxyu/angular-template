01166df29e4a84ebfea4cdaeb505b1382b445887
Wesley Braithwaite <wbraithwaite@Wesleys-MacBook-Pro.local>
2015-11-18 23:26:10 -0700

    fixed another mistake in routes

--- routes/sample.js
+++ routes/sample.js
@@ -11,20 +11,20 @@ var  sample = {
 		})
 	},
     getAll : function(req,res){
-		composerCollection.find({}, function(err, data){
+		sampleCollection.find({}, function(err, data){
 			res.json(data)
 		})
 	},
     getExactName : function(req, res){
 		var name = req.params.name
         var regex = new RegExp(["^", name, "$"].join(""), "i");
-		composerCollection.find({"name":regex}, function(err, data){
+		sampleCollection.find({"name":regex}, function(err, data){
 			res.json(data)
 		})
 	},
     addSampleItem : function(req, res){    
         var newItem = req.body   
-        composerCollection.insert(newItem, function(err, data){ 
+        sampleCollection.insert(newItem, function(err, data){ 
             if(data != undefined){
                 console.log("success!")
             } else {
@@ -35,7 +35,7 @@ var  sample = {
     updateSampleItem : function(req, res){
     	var updateItem = req.body
     	console.log(updateItem)
-    	composerCollection.update({"_id":updateItem._id}, updateItem, function(err, data){
+    	sampleCollection.update({"_id":updateItem._id}, updateItem, function(err, data){
     		console.log(updateItem)
     		res.json(updateItem)
     	})
